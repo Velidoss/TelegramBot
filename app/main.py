0173083269481b516371096ad17965e581cbbec8
Velidoss <godofthemountain09@gmail.com>
2019-10-18 22:24:45 +0300

    Подправил так, что теперь возвращается json файл нормальным образом)

--- app/main.py
+++ app/main.py
@@ -1,20 +1,27 @@
 import requests
+import json
 from flask import Flask
-from config.secret_token import TOKEN
+from secret_token import TOKEN
 
 
 """TODO
 1.прием сообщений
 2.отсылка сообщений
 """
+URL = f"https://api.telegram.org/bot{TOKEN}/"
+
+
+def write_json(data, filename = 'answer.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 
-URL = f"https://api.telegram.org/bot{TOKEN}"
+
 
 def main():
     r = requests.get(URL + 'getMe') #Добавляем метод getMe к api телеграма
-    print (r.json())
+    write_json (r.json())
 
 
 if __name__ == '__main__':
