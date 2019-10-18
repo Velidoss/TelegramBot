eedb32ecf49474631a0105204c8a85e80e1cffbd
Velidoss <godofthemountain09@gmail.com>
2019-10-18 22:46:25 +0300

    Бот принимает сообщения и отписывает фразу. Сдлеано через get_updates

--- app/main.py
+++ app/main.py
@@ -16,13 +16,26 @@ def write_json(data, filename = 'answer.json'):
         json.dump(data, f, indent=2, ensure_ascii=False)
 
 
+def get_updates():
+    url = URL+'getUpdates'#Добавляем метод getUpdates что бы получить овтет от телеграма
+    r = requests.get(url)
+    #write_json (r.json())
+    return r.json()
+#getUpdates возвращает обновления для бота за последние 24 часа
 
-
+def send_message(chat_id, text='bla-bla-bla'):
+    url=URL+'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text}
+    r = requests.post(url, json=answer)
+    return r.json()
 
 def main():
-    r = requests.get(URL + 'getMe') #Добавляем метод getMe к api телеграма
-    write_json (r.json())
-
+    #r = requests.get(URL + 'getMe') #Добавляем метод getMe к api телеграма
+    #write_json (r.json())
+    r = get_updates()
+    chat_id = r['result'][-1]['message']['chat']['id']
+    print(chat_id)
+    send_message(chat_id)
 
 if __name__ == '__main__':
    main()
